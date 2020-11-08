791ee74a2b6793731388570ff8a281c916fc7f0e
nophyon <juantago.games@gmail.com>
2020-11-08 09:54:40 +0100

    Table scaffold + webService

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from './index.js';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Table />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import $ from "jquery";
+
+const url = "https://swapi.dev/api/people/1";
+var swapi = null;
+
+var sample = {"name":"Luke Skywalker","height":"172","mass":"77","hair_color":"blond","skin_color":"fair","eye_color":"blue","birth_year":"19BBY","gender":"male","homeworld":"http://swapi.dev/api/planets/1/","films":["http://swapi.dev/api/films/1/","http://swapi.dev/api/films/2/","http://swapi.dev/api/films/3/","http://swapi.dev/api/films/6/"],"species":[],"vehicles":["http://swapi.dev/api/vehicles/14/","http://swapi.dev/api/vehicles/30/"],"starships":["http://swapi.dev/api/starships/12/","http://swapi.dev/api/starships/22/"],"created":"2014-12-09T13:50:51.644000Z","edited":"2014-12-20T21:17:56.891000Z","url":"http://swapi.dev/api/people/1/"}
+
+const element = <div>
+  <Table />
+</div>
+
+
+export function Table(){
+  getName(swapi);
+return <table>
+      <TableHeader />
+      <TableRow />
+       </table>
+}
+
+export function TableHeader(){
+  return <thead>
+    <tr>
+      <th>Name</th>
+      <th>Height</th>
+      <th>Mass</th>
+    </tr>
+  </thead>
+}
+
+export function TableRow(){
+  return <tr>
+      <td>{ sample.name }</td>
+  </tr>
+}
+
+
+function getName(){
+  
+    
+    // var thing = $.ajax(url);
+    // console.log(thing.responseText);
+    
+
+    const request = require('request');
+  request(url, function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('Name: '+ response)
+  swapi = body;
+  });
+    console.log("SHIT " + swapi);
+  }
+  
+  // console.log(thing);
+  // return thing.promise;
+  
+
+
+export default Table;
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
